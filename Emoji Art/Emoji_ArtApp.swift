8e8ac8cb6c7eed2e27d1c120d248b4729ff09af0
s1oly <90860963+s1oly@users.noreply.github.com>
2023-09-28 15:27:55 -0400

    Initial Commit

---
+++ Emoji Art/Emoji_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ArtApp.swift
+//  Emoji Art
+//
+//  Created by S1OLY on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
